6ec1350898a134f08ca658257e81c22e4230c736
SRIJAN-KUMAR7 <srijankumar77777@gmail.com>
2025-09-26 20:58:36 +0530

    testing some dependencies

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-print("hello world")
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    print("Starting Up.....")
+    #DB calls
+    #close calls
+    yield
+    print("Shutting Down....RAG.....")
+
+
+app=FastAPI(title="RAG for Quiz Microservice",lifespan=lifespan)
+
+@app.get('/')
+def read_root():
+    return {"message":"Welcome to RAG QUIZ Microservice"}
+
