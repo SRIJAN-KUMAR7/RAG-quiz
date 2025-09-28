a307ec6bf93be266d53c066ef5853ff76ba5f195
SRIJAN-KUMAR7 <srijankumar77777@gmail.com>
2025-09-28 14:34:08 +0530

    Main RAG with only working endpoints

--- app/main.py
+++ app/main.py
@@ -5,20 +5,35 @@ from app.api import upload
 from app.services.vector_db import init_pinecone
 from app.db.session import Base, engine
 import app.models.user, app.models.document, app.models.question, app.models.progress  # ensures models imported
+from app.api import questions
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     try:
         init_pinecone()
     except Exception as e:
-        print("⚠️ Pinecone init failed:", e)
+        print("Pinecone init failed:", e)
+    
+    from sqlalchemy import inspect
+    inspector = inspect(engine)
+    tables = inspector.get_table_names()
+    print(f"DEBUG: Existing database tables: {tables}")
+    
     Base.metadata.create_all(bind=engine)
+    
+    inspector = inspect(engine)
+    tables = inspector.get_table_names()
+    print(f"DEBUG: Tables after creation: {tables}")
+    
     yield
+
 
 app = FastAPI(title="RAG Quiz Microservice", lifespan=lifespan)
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
 
 app.include_router(upload.router, prefix="/upload", tags=["upload"])
+app.include_router(questions.router, prefix="/questions", tags=["questions"])  
+app.include_router(questions.router, prefix="/quiz", tags=["quiz"])  
 
 @app.get("/")
 def read_root():
